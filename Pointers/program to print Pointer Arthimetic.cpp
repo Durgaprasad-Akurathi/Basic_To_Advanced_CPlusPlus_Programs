93f4d43667adfb6e365f24b7c5cdc7cef5dd5276
Akurathi Durga Prasad <94420508+Durgaprasad-Akurathi@users.noreply.github.com>
2023-08-07 23:39:53 +0530

    Update program to print Pointer Arthimetic.cpp

--- Pointers/program to print Pointer Arthimetic.cpp
+++ Pointers/program to print Pointer Arthimetic.cpp
@@ -2,12 +2,18 @@
 using namespace std;
 int main()
 {
-    int A[5]={1, 2, 3, 4, 5};
+    int A[5]={5, 9, 7, 8, 2};
     int *p=A, *q=&A[4];
     
-    cout<<p++<<endl;
-    cout<<p--<<endl;
-    cout<<p+2<<endl;
-    cout<<p-2<<endl;
+    cout<<*p<<endl;
+    p++;
+    cout<<*p<<endl;
+    p--;
+    cout<<*p<<endl;
+    p+=2;
+    cout<<*p<<endl;
+    p-=2;
+    cout<<*p<<endl;
+    p++;
     cout<<q-p<<endl;
 }
