fa7d5e5a207d86fd16d35b9b4b5d42d93217f5d5
Akurathi Durga Prasad <94420508+Durgaprasad-Akurathi@users.noreply.github.com>
2023-08-21 15:49:05 +0530

    Update Program to perform Left Shift Operation in the given array.cpp

--- Arrays/1D Array/Program to perform Left Shift Operation in the given array.cpp
+++ Arrays/1D Array/Program to perform Left Shift Operation in the given array.cpp
@@ -17,3 +17,29 @@ int main()
     }
 
 }
+
+
+///////////////////////////////////////////////////////////////////////////////////////// Basic Method ////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,L;
+    cin>>n>>L;
+    int a[n];
+    for(int i=0; i<n; i++)
+    cin>>a[i];
+    int temp;
+    while(L--)
+    {
+        temp=a[0];
+        for(int i=0; i<n-1; i++)
+        a[i]=a[i+1];
+        a[n-1]=temp;
+    }
+
+    for(int i=0; i<n; i++)
+    cout<<a[i]<<" ";
+}
+
