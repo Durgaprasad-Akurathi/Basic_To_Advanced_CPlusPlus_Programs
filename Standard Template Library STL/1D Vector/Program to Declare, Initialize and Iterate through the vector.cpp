99e7664aeab4156cbe1c388749aaccebc41b17ad
Akurathi Durga Prasad <94420508+Durgaprasad-Akurathi@users.noreply.github.com>
2023-10-04 15:56:03 +0530

    Create Program to Declare, Initialize and Iterate through the vector.cpp

---
+++ Standard Template Library STL/1D Vector/Program to Declare, Initialize and Iterate through the vector.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(40);
+    v.push_back(50);
+    v.push_back(60);
+    v.push_back(70);
+    v.push_back(80);
+    v.push_back(90);
+    v.push_back(100);
+
+    for(auto x:v)
+    cout<<x<<" ";
+
+    // vector<int>:: iterator itr;
+    // for(itr=v.begin(); itr!=v.end(); itr++)
+    // cout<<*itr<<" ";
+}
