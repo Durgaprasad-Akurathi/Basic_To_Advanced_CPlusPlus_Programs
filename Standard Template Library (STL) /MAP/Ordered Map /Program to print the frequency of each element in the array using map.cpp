60bf3212f823980975c801fce45cd66126a5921e
Akurathi Durga Prasad <94420508+Durgaprasad-Akurathi@users.noreply.github.com>
2023-10-04 16:19:44 +0530

    Update Program to print the frequency of each element in the array using map.cpp

--- Standard Template Library (STL) /MAP/Ordered Map /Program to print the frequency of each element in the array using map.cpp
+++ Standard Template Library (STL) /MAP/Ordered Map /Program to print the frequency of each element in the array using map.cpp
@@ -7,11 +7,14 @@ int main()
     cin>>n;
     int a[n];
     for(int i=0; i<n; i++)
-    cin>>a[i];
-
-    for(int i=0; i<n; i++)
-    adp[a[i]]++;
-
+    {
+        cin>>a[i];
+        adp[a[i]]++;
+    }
+    
     for(auto x:adp)
     cout<<x.first<<" "<<x.second<<"\n";
+
+    // for(auto itr=adp.begin(); itr!=adp.end(); itr++)
+    // cout<<itr->first<<" "<<itr->second<<"\n";
 }
