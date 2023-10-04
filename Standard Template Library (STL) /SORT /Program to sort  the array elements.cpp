d9c38605221f8b3280bb03c1f91bfd53c6042f49
Akurathi Durga Prasad <94420508+Durgaprasad-Akurathi@users.noreply.github.com>
2023-10-04 17:15:48 +0530

    Update Program to sort  the array elements.cpp

--- Standard Template Library (STL) /SORT /Program to sort  the array elements.cpp
+++ Standard Template Library (STL) /SORT /Program to sort  the array elements.cpp
@@ -15,3 +15,21 @@ int main()
     for(int i=0; i<n; i++)
     cout<<a[i]<<" ";
 }
+
+//////////////////////////////////////////////////////////////////////////// Another Method /////////////////////////////////////////////////////////////////////////////
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++)
+    cin>>a[i];
+
+    sort(a, a+n);
+
+    for(int i=0; i<n; i++)
+    cout<<a[i]<<" ";
+}
