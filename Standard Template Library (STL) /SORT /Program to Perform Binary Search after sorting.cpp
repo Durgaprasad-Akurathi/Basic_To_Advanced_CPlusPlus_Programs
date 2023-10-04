5c026124547dc36cdcf4ab5ef8f984471f7f93af
Akurathi Durga Prasad <94420508+Durgaprasad-Akurathi@users.noreply.github.com>
2023-10-04 17:27:29 +0530

    Create Program to Perform Binary Search after sorting.cpp

---
+++ Standard Template Library (STL) /SORT /Program to Perform Binary Search after sorting.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a[1000001], n=0;
+    while(cin>>a[n])
+    {
+        if(cin.get()=='\t')
+        break;
+        n++;
+    }
+
+    sort(a, a+n);
+
+    for(int i=0; i<n; i++)
+    cout<<a[i]<<" ";
+    cout<<endl;
+
+    if(binary_search(a, a+n, 15))
+    {
+        int value=lower_bound(a, a+n, 15)-a;
+        cout<<"Present at Location: "<<value;
+    }
+    else cout<<"Not Present";
+}
+
+////////////////////////////////////////////////////////////////////////////////////////// Another Method ////////////////////////////////////////////////////////////////////
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++)
+    cin>>a[i];
+    
+    sort(a, a+n);
+
+    for(int i=0; i<n; i++)
+    cout<<a[i]<<" ";
+    cout<<endl;
+
+    if(binary_search(a, a+n, 15))
+    {
+        int value=lower_bound(a, a+n, 15)-a;
+        cout<<"Present at Location: "<<value;
+    }
+    else cout<<"Not Present";
+}
