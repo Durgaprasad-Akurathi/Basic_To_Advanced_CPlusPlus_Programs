2407f378d3bf44ded4cfeae5b8f7c7bd92d1eea0
Akurathi Durga Prasad <94420508+Durgaprasad-Akurathi@users.noreply.github.com>
2023-10-09 15:50:07 +0530

    Create Program to take input from user into vector.cpp

---
+++ Standard Template Library (STL) /Vectors /1D Vector /Program to take input from user into vector.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a;
+    vector<int> v;
+    for(int i=0; i<n; i++)
+    {
+        cin>>a;
+        v.push_back(a);
+    }
+
+    for(auto &x:v)
+    cout<<x<<" ";
+}
