61a7c317389801dff2aa1f60c3a56b82cdd938e5
Akurathi Durga Prasad <94420508+Durgaprasad-Akurathi@users.noreply.github.com>
2023-10-10 10:14:30 +0530

    Update Program to Declare, Initialize & Iterates through the given vector.cpp

--- Standard Template Library (STL) /Vectors /1D Vector /Program to Declare, Initialize & Iterates through the given vector.cpp
+++ Standard Template Library (STL) /Vectors /1D Vector /Program to Declare, Initialize & Iterates through the given vector.cpp
@@ -22,3 +22,22 @@ int main()
     // for(auto x:v)
     // cout<<x<<" ";
 }
+
+
+/////////////////////////////////////////////////////////////////////////////// ANOTHER TYPE ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(30);
+    v.push_back(50);
+    for(int i=0; i<v.size(); i++)
+    cout<<v[i]<<" ";   //This will not the check the array out of bound index & it will not through any exception like out of bound.
+
+    for(int i=0; i<v.size(); i++)
+    cout<<v.at(i)<<" ";    //This will check the array out of bound index & it will through exception like out of bound.It is better to use this syntax to avoid runtime error.
+}
