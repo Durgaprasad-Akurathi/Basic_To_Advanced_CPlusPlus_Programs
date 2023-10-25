3f9f3d7a51bc9db7def1c8d79b81e6a43c19f827
Akurathi Durga Prasad <94420508+Durgaprasad-Akurathi@users.noreply.github.com>
2023-10-26 00:39:20 +0530

    Update Program to perform Right Shift Operation of the given array.cpp

--- Arrays/1D Array/Program to perform Right Shift Operation of the given array.cpp
+++ Arrays/1D Array/Program to perform Right Shift Operation of the given array.cpp
@@ -17,3 +17,35 @@ int main()
     }
 
 }
+
+///////////////////////////////////////////////////////////////////////////////// Another Method ///////////////////////////////////////////////////////////////////////////////
+
+#include "bits/stdc++.h"
+using namespace std;
+int main()
+{
+    int n, RightShift;
+    cout<<"Enter the size of the array:";
+    cin>>n;
+    cout<<"Enter how many right shift operation you want to perform:";
+    cin>>RightShift;
+    int a[n];
+    cout<<"Enter the array elements:";
+    for(int i=0; i<n; i++)
+    cin>>a[i];
+
+    while(RightShift--)
+    {
+        int temp=a[n-1];
+        for(int i=n-1; i>=0; i--)
+            a[i+1]=a[i];
+        a[0]=temp;
+    }
+
+    for(int i=0; i<n; i++)
+        cout<<a[i]<<" ";
+}
+
+    for(int i=0; i<n; i++)
+        cout<<v[i]<<" ";
+}
